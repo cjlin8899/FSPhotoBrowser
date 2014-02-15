28716211af31e179e568452bf01cd917dab5fcf3
封禅 <yangfengshan@gmail.com>
2014-02-15 19:33:46 +0800

    Create project.
    Create project.

---
+++ FSPhotoBrowser/FSPhotoBrowser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FSPhotoBrowser
+//
+//  Created by Fengshan.yang on 14-2-15.
+//  Copyright (c) 2014年 Fanyi Network techonology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FSPhotoBrowser/FSPhotoBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FSPhotoBrowser
+//
+//  Created by Fengshan.yang on 14-2-15.
+//  Copyright (c) 2014年 Fanyi Network techonology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
